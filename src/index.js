eef59347fc384468f994a4d99114fbdd25872202
Nguyen Dinh Nhan <nhan.nguyendinh@petronas.com>
2021-11-02 13:21:10 +0700

    prettier: result

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // body-parser
-app.use(express.urlencoded({extended: true})) // <form />
-app.use(express.json()) // XMLHttpRequest, fetch, axios, ajax...
+app.use(express.urlencoded({ extended: true })); // <form />
+app.use(express.json()); // XMLHttpRequest, fetch, axios, ajax...
 
 // HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars({extname: ".hbs"}))
-app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources/views'))
+app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 // Initial Routes
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
