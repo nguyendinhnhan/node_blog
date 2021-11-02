b1236221c352ac12ad1ea4779d7a9f1f8ef445ee
Nguyen Dinh Nhan <nhan.nguyendinh@petronas.com>
2021-11-02 12:18:24 +0700

    site route & controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,11 @@
 const newsRouter = require("./news");
+const siteRouter = require("./site");
 
 function route(app) {
-  app.get("/", (req, res) => {
-    // res.send('Hello World!')
-    res.render("home");
-  });
 
-  // app.get("/news", (req, res) => {
-  //   res.render("news");
-  // });
   app.use("/news", newsRouter);
 
-  app.get("/search", (req, res) => {
-    res.render("search");
-  });
+  app.use("/", siteRouter);
 
   app.post("/search", (req, res) => {
     console.log(req.body);
