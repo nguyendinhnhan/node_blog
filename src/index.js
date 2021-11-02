b43008eae3d5a0112d4e801abc973495dd6a129e
Nguyen Dinh Nhan <nhan.nguyendinh@petronas.com>
2021-11-02 14:12:29 +0700

    format code

--- src/index.js
+++ src/index.js
@@ -11,14 +11,14 @@ const route = require('./routes');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // body-parser
-app.use(express.urlencoded({ extended: true })); // <form />
+app.use(express.urlencoded({extended: true})); // <form />
 app.use(express.json()); // XMLHttpRequest, fetch, axios, ajax...
 
 // HTTP logger
 app.use(morgan('combined'));
 
 // Template engine
-app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.engine('hbs', handlebars({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
@@ -26,5 +26,5 @@ app.set('views', path.join(__dirname, 'resources/views'));
 route(app);
 
 app.listen(port, () => {
-          console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
