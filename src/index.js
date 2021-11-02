079ff828f14a83059ecba18df2c8729104e4bcd3
Nguyen Dinh Nhan <nhan.nguyendinh@petronas.com>
2021-11-02 12:10:54 +0700

    News route & controler

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 // body-parser
@@ -20,23 +22,8 @@ app.engine('hbs', handlebars({extname: ".hbs"}))
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-  // res.send('Hello World!')
-  res.render('home')
-})
-
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-
-app.get('/search', (req, res) => {
-  res.render('search')
-})
-
-app.post('/search', (req, res) => {
-  console.log(req.body)
-  res.send('')
-})
+// Initial Routes
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
---
+++ src/routes/index.js
@@ -0,0 +1,24 @@
+const newsRouter = require("./news");
+
+function route(app) {
+  app.get("/", (req, res) => {
+    // res.send('Hello World!')
+    res.render("home");
+  });
+
+  // app.get("/news", (req, res) => {
+  //   res.render("news");
+  // });
+  app.use("/news", newsRouter);
+
+  app.get("/search", (req, res) => {
+    res.render("search");
+  });
+
+  app.post("/search", (req, res) => {
+    console.log(req.body);
+    res.send("");
+  });
+}
+
+module.exports = route;
